9755a04937b4f16c6fae59f36c805a51a6814359
Becker Michael <Michael.Becker@bystronic.com>
2025-09-16 09:05:19 +0200

    teständerung

--- src/01_grundlagen/beispiele/hello_world.py
+++ src/01_grundlagen/beispiele/hello_world.py
@@ -12,7 +12,7 @@ def main():
 
     # Einfache Ausgabe
     print("=" * 50)
-    print("Willkommen bei Python!")
+    print("Willkommen bei Python2!")
     print("=" * 50)
 
     # Variablen definieren
